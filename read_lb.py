36dc7a54c3a79560bcd5a896e1667a40adaf122f
Robert Havelka <rhavelka@moravia.com>
2016-09-19 11:02:18 +0200

    less workers, cleanup cfg

--- read_lb.py
+++ read_lb.py
@@ -7,23 +7,25 @@ import pprint
 import requests
 
 TARGET_URL = "http://192.168.99.100/"
-COUNTED_STATS = {}
+REPORT = {}
 
 _CNT_INIT = 50
 _ERROR_ID = "Error"
 
-COUNTED_STATS[_ERROR_ID] = 0  # initialize Errors (when response status code is not 200)
-CNT = _CNT_INIT
+REPORT[_ERROR_ID] = 0  # initialize Errors (when response status code is not 200)
+print_cnt = _CNT_INIT
 
 while True:
     response = requests.get(TARGET_URL)
 
     if response.status_code != 200:
-        COUNTED_STATS[_ERROR_ID] += 1
+        REPORT[_ERROR_ID] += 1
     else:
-        COUNTED_STATS[response.text] = COUNTED_STATS.get(response.text, 0) + 1
+        REPORT[response.text] = REPORT.get(response.text, 0) + 1
 
-    CNT -= 1
-    if not CNT:
-        CNT = _CNT_INIT
-        pprint.pprint(COUNTED_STATS)
+    response.close()  # Explicit calling due intermittent resource exhaustion
+
+    print_cnt -= 1
+    if not print_cnt:
+        pprint.pprint(REPORT)
+        print_cnt = _CNT_INIT
